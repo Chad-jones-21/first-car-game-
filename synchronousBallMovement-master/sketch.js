e49410db2fb9eb0d5ddac049130c1afa6c071b67
Chad-jones-21 <76543990+Chad-jones-21@users.noreply.github.com>
2021-03-21 22:36:46 -0400

    Add files via upload

---
+++ synchronousBallMovement-master/sketch.js
@@ -0,0 +1,22 @@
+var hypnoticBall, database;
+var position;
+var gameState = 0
+var form
+var game
+
+function setup(){
+  database = firebase.database();
+
+  createCanvas(500,500);
+game= new Game()
+game.getState(
+  game.start()
+)
+
+}
+
+function draw(){
+ 
+  
+}
+
